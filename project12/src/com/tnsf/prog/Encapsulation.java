37a36d613ecae5abe2bddf8db3a1def50238118e
acer <acer@TMP249-G2-M.bbrouter>
2023-09-22 14:42:06 +0530

    secound commit

---
+++ project12/src/com/tnsf/prog/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tnsf.prog;
+
+public class Encapsulation {
+
+	public int serialno;
+	public String name;
+	public int age;
+	public void show()
+	{
+	System.out.println("serialno="+serialno+"\t"+"name="+name+"\t"+"age="+age);
+
+	}
+
+}
+
---
+++ project12/src/com/tnsf/prog/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsf.prog;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob= new Encapsulation();
+		ob.serialno=12;
+		ob.name="anil";
+		ob.age=21;
+		ob.show();
+	}
+
+}
