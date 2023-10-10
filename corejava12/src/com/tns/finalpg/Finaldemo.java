b6c1f38dfcf7b126d2ad83998f67f1d509a530f8
acer <acer@10.0.0.148>
2023-10-10 15:41:23 +0530

    commit

---
+++ corejava12/src/com/tns/finalpg/Finaldemo.java
@@ -0,0 +1,17 @@
+package com.tns.finalpg;
+
+public class Finaldemo {
+		int a=20;
+		void display() {
+			System.out.println("the value of a is"+a);
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+		
+		Finaldemo ob= new Finaldemo();
+		ob.display();
+		}
+
+	}
+
